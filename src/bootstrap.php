b3150e20ef3e4f8a88c3a83f19bfdcabfc107992
Sebastian Nepote <snepote@gmail.com>
2015-04-06 18:27:43 +0000

    Enable phpunit on travis and adds dummy unit test

---
+++ src/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Application bootstrap
+ *
+ * PHP version ~5.4
+ *
+ * @author Sebastian Nepote <snepote@gmail.com>
+ * @since  0.0.1
+ */
+
+date_default_timezone_set('Europe/Berlin');
+include dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
---
+++ test/unit/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Unit Test bootstrap
+ *
+ * PHP version ~5.4
+ *
+ * @author Sebastian Nepote <snepote@gmail.com>
+ */
+
+include dirname(dirname(dirname(__FILE__))) . DIRECTORY_SEPARATOR . 'src/bootstrap.php';
