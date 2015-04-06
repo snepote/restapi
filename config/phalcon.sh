5efc8cd43b4574da9374ff9ca64a034ce4c7d0f4
Sebastian Nepote <snepote@gmail.com>
2015-04-06 17:51:57 +0000

    phalcon install, no verbose except errors

--- config/phalcon.sh
+++ config/phalcon.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 sudo apt-add-repository ppa:phalcon/stable
-sudo apt-get update -y -f
-sudo apt-get install python-software-properties -y -f
-sudo apt-get install php5-phalcon -y -f
-# sudo cp -f ./config/phalcon/phalcon.ini /etc/php5/mods-available/phalcon.ini
-# sudo ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/fpm/conf.d/20-phalcon.ini
+sudo apt-get update -y -f -qq
+sudo apt-get install python-software-properties -y -f -qq
+sudo apt-get install php5-phalcon -y -f -qq
