42181f7d4bc4ed2f6c04acef3a37db33951f9ad8
Sebastian Nepote <snepote@gmail.com>
2015-04-06 18:02:34 +0000

    fix apt-get wrong parameter

--- config/phalcon.sh
+++ config/phalcon.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo apt-add-repository ppa:phalcon/stable
-sudo apt-get update -y -f -qq
-sudo apt-get install python-software-properties -y -f -qq
-sudo apt-get install php5-phalcon -y -f -qq
+sudo apt-add-repository ppa:phalcon/stable -y
+sudo apt-get update -y -qq
+sudo apt-get install python-software-properties -y -qq
+sudo apt-get install php5-phalcon -y -qq
