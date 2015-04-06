98e4fecfc16d200615a1ef53dd7e8eca2666269f
Sebastian Nepote <snepote@gmail.com>
2015-04-06 17:43:03 +0000

    phalcon config no prompt

--- config/phalcon.sh
+++ config/phalcon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sudo apt-add-repository ppa:phalcon/stable
-sudo apt-get update
-sudo apt-get install python-software-properties
-sudo apt-get install php5-phalcon
+sudo apt-add-repository ppa:phalcon/stable -y -f
+sudo apt-get update -y -f
+sudo apt-get install python-software-properties -y -f
+sudo apt-get install php5-phalcon -y -f
 # sudo cp -f ./config/phalcon/phalcon.ini /etc/php5/mods-available/phalcon.ini
 # sudo ln -s /etc/php5/mods-available/phalcon.ini /etc/php5/fpm/conf.d/20-phalcon.ini
